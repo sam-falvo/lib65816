d4b6a97fc9133edce2225c71b9b77bfce677f0e9
kc5tja <kc5tja@localhost.localdomain>
2008-11-03 17:05:02 -0800

    Initial import from Darcs repository.

---
+++ config.mk
@@ -0,0 +1,11 @@
+CC      := gcc
+CCFLAGS := -c -O2 -fomit-frame-pointer
+LD      := gcc
+LDFLAGS :=
+
+PREFIX  := /Programs/Kestrel/1
+LIBPATH := $(PREFIX)/lib
+INCPATH := $(PREFIX)/include
+
+DELETE  := rm -rf
+
